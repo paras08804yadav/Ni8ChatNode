e0b6f38173d4dcbb311a0de73222ce7ce8ae7c4b
paras08804yadav <parasyadav0502@gmail.com>
2025-01-17 14:45:46 +0530

    fourth commit

--- index3.js
+++ index3.js
@@ -51,7 +51,7 @@ app.use('/api/hosts', hostRoutes);
 app.use('/api/agencies', agencyRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const HOST = 'localhost';
 
 server.listen(PORT, HOST, () => {
